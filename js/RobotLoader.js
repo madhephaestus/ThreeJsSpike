c9e3a54194a92d1302534c2a756f4a4506e83b0b
Kevin Harrington <kharrington@softroboticsinc.com>
2023-01-09 16:28:10 -0500

    updating the basicic models

--- js/RobotLoader.js
+++ js/RobotLoader.js
@@ -1,22 +1,62 @@
 import { OrbitControls } from './OrbitControls.js'; 
+import { STLLoader } from './STLLoader.js'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 
+scene.add(new THREE.AxesHelper(5))
+const light2 = new THREE.SpotLight()
+light2.position.set(100, 100, 100)
+scene.add(light2)
+
+const light3 = new THREE.SpotLight()
+light3.position.set(-100, 100, 100)
+scene.add(light3)
+
+const light4 = new THREE.SpotLight()
+light4.position.set(100, -100, 100)
+scene.add(light4)
+
+const light = new THREE.SpotLight()
+light.position.set(-100, -100, 100)
+scene.add(light)
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var mesh 
+const material = new THREE.MeshPhongMaterial( { color: 0x0000FF } );
 
-camera.position.set( 0, 0, 10 );
+const loader = new STLLoader()
+loader.load(
+    '../assets/fingertip.stl',
+    function (geometry) {
+        mesh = new THREE.Mesh(geometry, material)
+        scene.add(mesh)
+        animate();
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
+
+camera.position.set( 0, 0, 100 );
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.update();
 
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
+function render() {
+    renderer.render(scene, camera)
+}
 function animate() {
 	requestAnimationFrame( animate );
 
@@ -24,9 +64,9 @@ function animate() {
 	controls.update();
 	
 	//cube.rotation.x += 0.01;
-	cube.rotation.z += 0.01;
+	mesh.rotation.z += 0.01;
 
-	renderer.render( scene, camera );
+	render();
 };
 
-animate();
+window.addEventListener('resize', onWindowResize, false)
