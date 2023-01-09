ed82768f865045c876f6d112dfe8e4b8cec2d9b1
Kevin Harrington <kharrington@softroboticsinc.com>
2023-01-09 15:31:29 -0500

    basic orbital control

--- js/RobotLoader.js
+++ js/RobotLoader.js
@@ -1,6 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.124/build/three.module.js'; 
-
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/controls/OrbitControls.js'; 
+import { OrbitControls } from './OrbitControls.js'; 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
