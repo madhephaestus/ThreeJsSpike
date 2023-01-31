c8f6c4167b5ceb6cbda274feae6c9b6458e89adf
Kevin Harrington <kharrington@softroboticsinc.com>
2023-01-31 10:24:50 -0500

    adding a structure for the number of fingers

--- js/RobotLoader.js
+++ js/RobotLoader.js
@@ -1,11 +1,11 @@
-import { OrbitControls } from './OrbitControls.js'; 
+import { OrbitControls } from './OrbitControls.js';
 import { STLLoader } from './STLLoader.js'
 import { GUI } from './lil-gui.module.min.js';
 import { DHStep } from './DHChain.js';
 import { DHChain } from './DHChain.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 
 scene.add(new THREE.AxesHelper(5))
@@ -25,105 +25,107 @@ const light = new THREE.SpotLight()
 light.position.set(-100, -100, 100)
 scene.add(light)
 
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const fingers=3;
-const bellows=2;
-const type="T";
-const circular=false;
+const fingers = 3;
+const bellows = 2;
+const type = "T";
+const circular = false;
 const seperationDist = 60;
+const fingerList = [];
 
-const links = [];
-links.push(new DHStep(20,0,0,0))
-for(let i=0; i<bellows; i+=1){
-	links.push(new DHStep(30,0,0,0))
+for (let j = 0; j < fingers; j += 1) {
+	var links = [];
+	links.push(new DHStep(20, 0, 0, 0))
+	for (let i = 0; i < bellows; i += 1) {
+		links.push(new DHStep(30, 0, 0, 0))
+	}
+	links.push(new DHStep(40, 0, 0, 0))
+	fingerList.push(new DHChain(new THREE.Matrix4(), new THREE.Matrix4(), links))
 }
-links.push(new DHStep(40,0,0,0))
-const chain = new DHChain(new THREE.Matrix4(),new THREE.Matrix4(),links)
-
-var tip,rib,base 
-const material = new THREE.MeshPhongMaterial( { color: 0x0000FF } );
+var tip, rib, base
+const material = new THREE.MeshPhongMaterial({ color: 0x0000FF });
 const loader = new STLLoader()
 let startState = [];
-for(let i=0; i<3; i+=1){
-    startState.push(false);
+for (let i = 0; i < 3; i += 1) {
+	startState.push(false);
 }
 loader.load(
-    '../assets/fingertip.stl',
-    function (geometry) {
-        tip = new THREE.Mesh(geometry, material)
-        scene.add(tip)
-        start(0)
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
+	'../assets/fingertip.stl',
+	function(geometry) {
+		tip = new THREE.Mesh(geometry, material)
+		scene.add(tip)
+		start(0)
+	},
+	(xhr) => {
+		console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+	},
+	(error) => {
+		console.log(error)
+	}
 )
 loader.load(
-    '../assets/fingerRib.stl',
-    function (geometry) {
-        rib = new THREE.Mesh(geometry, material)
-        scene.add(rib)
-        start(1)
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
+	'../assets/fingerRib.stl',
+	function(geometry) {
+		rib = new THREE.Mesh(geometry, material)
+		scene.add(rib)
+		start(1)
+	},
+	(xhr) => {
+		console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+	},
+	(error) => {
+		console.log(error)
+	}
 )
 loader.load(
-    '../assets/fingerBase.stl',
-    function (geometry) {
-        base = new THREE.Mesh(geometry, material)
-        scene.add(base)
-        start(2)
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
+	'../assets/fingerBase.stl',
+	function(geometry) {
+		base = new THREE.Mesh(geometry, material)
+		scene.add(base)
+		start(2)
+	},
+	(xhr) => {
+		console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+	},
+	(error) => {
+		console.log(error)
+	}
 )
 
-camera.position.set( 0, 0, 100 );
-const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.set(0, 0, 100);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	render()
 }
 function render() {
-    renderer.render(scene, camera)
+	renderer.render(scene, camera)
 }
 
 function start(myIndex) {
-	startState[myIndex]=true;
-	for(let i=0; i<3; i+=1){
-	    if (!startState[i])
-	   	 return
+	startState[myIndex] = true;
+	for (let i = 0; i < 3; i += 1) {
+		if (!startState[i])
+			return
 	}
-    animate();
+	animate();
 }
 function animate() {
-	requestAnimationFrame( animate );
+	requestAnimationFrame(animate);
 
 	// required if controls.enableDamping or controls.autoRotate are set to true
 	controls.update();
-	
+
 	//cube.rotation.x += 0.01;
 	tip.rotation.z += 0.01;
 	rib.rotation.y += 0.01;
 	base.rotation.x += 0.01;
-	
+
 	render();
 };
 
